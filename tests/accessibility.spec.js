e27350c9b712d7682a18ded316c4ca415d38df43
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-10-03 15:01:11 +0530

    implemented a11y basic test

---
+++ tests/accessibility.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { AxeBuilder } from '@axe-core/playwright';
+
+test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['best-practice'])
+      // implemented best-practice testing tag to avoid most of the accessibility issues
+      // other alternatives are present below, uncomment to use
+      // .withTags(['wcag2a', 'wcag2aa'])
+      .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
