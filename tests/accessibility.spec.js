8f37e9336801191f90667c7335f0d73edc5c18ea
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-10-03 15:09:16 +0530

    readme updated

--- tests/accessibility.spec.js
+++ tests/accessibility.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { AxeBuilder } from '@axe-core/playwright';
 
-test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+test('should not have any automatically detectable accessibility violations', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   const accessibilityScanResults = await new AxeBuilder({ page })
