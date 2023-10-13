17a06a3812d9c289e4f58e8aef53f36693351180
Chizycodes <okwudirejoy@gmail.com>
2023-10-13 18:01:27 +0100

    [BUG] A11y - Ensures every HTML document has a lang attribute

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function MyDocument() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default MyDocument;
