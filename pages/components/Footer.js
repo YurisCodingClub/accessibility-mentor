47737b836c7e20720a703ed5573707a03c5a9077
Christine Belzie <shecoder30@gmail.com>
2023-10-19 19:08:40 -0400

    feat: imported logo

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
-// import logo here 
+import logo from '../../../assets/logo.png';
 
 
 const Footer = () => {
     return (
         <footer style={{ width: "100%", position: "relative", bottom: "0"}}>
+            <Image src={Logo} alt="footer-logo"></Image>
         </footer>
     );
 };
