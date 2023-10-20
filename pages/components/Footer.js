f215aace285990a020033155d727b8ca49164e48
Christine <shecoder30@gmail.com>
2023-10-20 19:11:16 +0000

    fix: revise import name

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -6,7 +6,7 @@ import logo from '../../../assets/logo.png';
 const Footer = () => {
     return (
         <footer style={{ width: "100%", position: "relative", bottom: "0"}}>
-            <Image src={Logo} alt="footer-logo" width={100} height={100}></Image>
+            <Image src={logo} alt="footer-logo" width={100} height={100}></Image>
             <p className="copyright"> AccessibilityMentor 2023. All rights reserved.</p>
         </footer>
     );
