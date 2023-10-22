11f425c91c47fe4870fb65eb6c86f41d57394f24
Christine Belzie <shecoder30@gmail.com>
2023-10-21 20:33:12 -0400

    fix: moved logo and renamed file path

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import logo from '../../public/images/logo.png';
+import logo from '../../../assets/logo.png';
 
 
 const Footer = () => {
