d127280f8964d9be412726c35c5f64c36f17b0e7
Christine Belzie <shecoder30@gmail.com>
2023-10-21 20:39:04 -0400

    fix: revise file path

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import logo from '../../../assets/logo.png';
+import logo from '../../stories/assets/logo.png';
 
 
 const Footer = () => {
