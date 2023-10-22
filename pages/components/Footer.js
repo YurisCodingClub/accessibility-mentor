c46ebfaae208372730b8407aa73c48e4277a2fc1
Christine Belzie <shecoder30@gmail.com>
2023-10-21 20:47:03 -0400

    fix: replace CSS property

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -5,7 +5,7 @@ import logo from '../../stories/assets/logo.png';
 
 const Footer = () => {
     return (
-        <footer style={{ width: "100%", position: "relative", bottom: "0"}}>
+        <footer style={{ width: "100%", position: "fixed", bottom: "0"}}>
             <Image src={logo} alt="footer-logo" width={100} height={100}/>
             <p className="copyright"> AccessibilityMentor 2023. All rights reserved.</p>
         </footer>
