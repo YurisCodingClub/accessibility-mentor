5f78e4a5ef404cea2297b9b2d63fe27739e4fccf
Christine Belzie <shecoder30@gmail.com>
2023-10-22 14:18:50 -0400

    fix: add margin and text-align center

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -4,7 +4,15 @@ import logo from "../../stories/assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer style={{ width: "100%", position: "fixed", bottom: "0" }}>
+    <footer
+      style={{
+        width: "100%",
+        position: "fixed",
+        bottom: "0",
+        margin: "auto",
+        textAlign: "center",
+      }}
+    >
       <Image src={logo} alt="footer-logo" width={100} height="auto" />
       <p className="copyright">
         AccessibilityMentor 2023. All rights reserved.
