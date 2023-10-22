929f80626a388830c76efdd91366fb33dd7723f3
Christine Belzie <shecoder30@gmail.com>
2023-10-21 21:20:12 -0400

    fix: revise height property to auto

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import logo from '../../stories/assets/logo.png';
-
+import logo from "../../stories/assets/logo.png";
 
 const Footer = () => {
-    return (
-        <footer style={{ width: "100%", position: "fixed", bottom: "0"}}>
-            <Image src={logo} alt="footer-logo" width={100} height={100}/>
-            <p className="copyright"> AccessibilityMentor 2023. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer style={{ width: "100%", position: "fixed", bottom: "0" }}>
+      <Image src={logo} alt="footer-logo" width={100} height="auto" />
+      <p className="copyright">
+        AccessibilityMentor 2023. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
